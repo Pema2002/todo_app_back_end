cd5625f6c760f0dae22b5c813515c15a7caff55c
Pema Lama <lamap@bxscience.edu>
2018-05-12 19:22:00 +0000

    make conditionals

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -10,6 +10,21 @@ class TodoController < ApplicationController
           elsif todo_id == '2'
             @todo_description = "Buy workshop supplies"
             @todo_pomodoro_estimate = 3
+         elsif todo_id == '3' 
+            @todo_description = "S"
+            @todo_pomodoro_estimate = 4
+        elsif todo_id == '4'
+        	@todo_description = "Meet with the volunteer trainers"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '5'
+        	@todo_description = "Order the food for saturday and sunday"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '6'
+        	@todo_description = "check pre work assignments"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '7'
+        	@todo_description = "Have a great workshop"
+        	@todo_pomodoro_estimate = 2
         end
     end
 end
