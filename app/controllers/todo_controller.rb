6794352636e663c2738b8e4e469b92c79a7f3194
Pema Lama <lamap@bxscience.edu>
2018-05-13 15:28:33 +0000

    pulling straight from the database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,30 +2,8 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        todo_id = params[:id]
-        if todo_id == '1'
-            @todo_description = "Make the curriculum"
-            @todo_pomodoro_estimate = 4
+        @todo = Todo.find_by_id(params[:id])
     
-          elsif todo_id == '2'
-            @todo_description = "Buy workshop supplies"
-            @todo_pomodoro_estimate = 3
-         elsif todo_id == '3' 
-            @todo_description = "S"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id == '4'
-        	@todo_description = "Meet with the volunteer trainers"
-        	@todo_pomodoro_estimate = 2
-        elsif todo_id == '5'
-        	@todo_description = "Order the food for saturday and sunday"
-        	@todo_pomodoro_estimate = 2
-        elsif todo_id == '6'
-        	@todo_description = "check pre work assignments"
-        	@todo_pomodoro_estimate = 2
-        elsif todo_id == '7'
-        	@todo_description = "Have a great workshop"
-        	@todo_pomodoro_estimate = 2
-        end
     end
 end
 
